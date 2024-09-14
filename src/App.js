5d6b25453194eff6f1176b22f7ff17fea3bd169e
jeremias210421 <jeremias210421@gmail.com>
2024-09-14 16:20:19 -0300

    Update and rename app.js to App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WhatsAppChatbotConfigurator from './components/WhatsAppChatbotConfigurator';
+
+const App = () => {
+  return (
+    <div className="App">
+      <WhatsAppChatbotConfigurator />
+    </div>
+  );
+};
+
+export default App;
