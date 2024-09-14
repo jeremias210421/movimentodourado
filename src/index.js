748029ef9db238b45b348497ff51f05729f7805c
jeremias210421 <jeremias210421@gmail.com>
2024-09-14 16:19:54 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './styles.css'; // Importa os estilos globais
+import './styles.css';
 
 ReactDOM.render(
   <React.StrictMode>
